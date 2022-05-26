e3b0b83f8e2303a6430f0245837f1f7779ae4e87
Juan Carlos Valencia Lizardi <86799485+jcgeneration@users.noreply.github.com>
2022-05-26 16:33:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ preguntaNums();
 
 //*1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
 //*2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
-// 3. Suspendido
+// 3. Adivinar un número entre el 1 y el 100 en el menor número de pasos posibles
 //*4. Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
 //*5. Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
 //*6. Elabora un algoritmo para leer un número y determinar si es par o impar
