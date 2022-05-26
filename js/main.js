7208d436793d259765ff48566121050059a1115c
jc <juan.valencia@idr.edu.mx>
2022-05-26 16:24:44 -0500

    Se agregó la función para pedir números

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+function preguntaNums(){
+    let num1 = prompt("Dime el primer número","");
+    let num2 = prompt("Dime el segundo número","");
+    let num3 = prompt("Dime el tercer número","");
+
+}// preguntaNums
+
+preguntaNums();
+
 //*1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
 //*2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
-
 // 3. Suspendido
-
 //*4. Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
 //*5. Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
 //*6. Elabora un algoritmo para leer un número y determinar si es par o impar
