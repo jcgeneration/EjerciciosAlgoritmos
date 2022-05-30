d1378761206c520d9c649823b6ffd07708dee420
jc <juan.valencia@idr.edu.mx>
2022-05-30 11:34:21 -0500

    Se agregó la solución del algoritmo #3

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function preguntaNums(){
 
 }// preguntaNums
 
-preguntaNums();
+//preguntaNums();
 
 //*1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
 //*2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
@@ -16,3 +16,29 @@ preguntaNums();
 
 
 // Terminar los ejercicios 1, 2, 4, 5 y 6
+
+// 1  100   50   89    99   8
+function Adivinar() {
+    let superior = 100;
+    let inferior = 0;
+    let noEncontrado = true;
+    while(noEncontrado) {
+        let mid = parseInt(inferior + ((superior-inferior)/ 2));
+        console.log(mid);
+        console.log(((superior-inferior)/ 2));
+        if (((superior-inferior)/ 2)<1){
+            noEncontrado=false;
+            alert("Tu número es el " + (parseInt(mid)+1));
+            break;
+        }//if
+        let res = confirm("Tu número es menor o igual a " + mid);
+        if (res) {
+            superior=mid;
+        } else {
+            inferior=mid;
+        }//if 
+        console.log(inferior, superior);
+    } //while
+} //Adivinar
+
+Adivinar();
